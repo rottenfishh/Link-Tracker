07e8cd89ace207b0a5e71ef740604c6b81f6b6e5
Алина Колодина <a.kolodina@g.nsu.ru>
2026-02-18 18:48:41 +0700

    delete dispatcher

--- cmd/bot/registry.go
+++ cmd/bot/registry.go
@@ -1,5 +1,11 @@
 package bot
 
+import (
+	"context"
+	"fmt"
+	"strings"
+)
+
 type Registry struct {
 	cmds map[string]Command
 }
@@ -22,4 +28,17 @@ func (r *Registry) Get(name string) Command {
 
 func (r *Registry) GetCommands() map[string]Command {
 	return r.cmds
+}
+
+func (r *Registry) Dispatch(ctx *context.Context, command string) (*Message, error) {
+	args := strings.Split(command, " ")
+	if len(args) < 1 {
+		return nil, fmt.Errorf("empty command")
+	}
+	cmd, _ := r.cmds[args[0]]
+	msg, err := cmd.Execute(ctx, args[1:])
+	if err != nil {
+		return nil, err
+	}
+	return msg, nil
 }
